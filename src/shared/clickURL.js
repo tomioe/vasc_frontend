a717d439ed628be30819bf5eb3898d84bf9e9e70
Tobias Østergren <tobias.oestergren@ieee.org>
2021-03-18 23:08:44 +0100

    fixed click

--- src/shared/clickURL.js
+++ src/shared/clickURL.js
@@ -6,6 +6,6 @@ export const redirectToClickURL = (e, vendorURL) => {
     e.preventDefault();
     const base64URL = new Buffer(vendorURL).toString('base64');
     const clientDeviceData = new Buffer( JSON.stringify(deviceDetect()) ).toString('base64')
-    console.log(`${API_BASE_URL}/click?url=${base64URL}&cdd=${clientDeviceData}`);
-    // window.open(`${API_BASE_URL}/click?url=${base64URL}&cdd=${clientDeviceData}`, '_blank');
+    // console.log(`${API_BASE_URL}/click?url=${base64URL}&cdd=${clientDeviceData}`);
+    window.open(`${API_BASE_URL}/click?url=${base64URL}&cdd=${clientDeviceData}`, '_blank');
 };
